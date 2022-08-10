a1230873d284d02c77696dcd5a926c14a90bcc96
erickz5501 <54655666+erickz5501@users.noreply.github.com>
2022-08-10 18:21:46 -0500

    Componente editar producto

---
+++ resources/views/livewire/admin/admin-edit-product-component.blade.php
@@ -0,0 +1,98 @@
+<div>
+    <div class="container" style="padding: 30px 0 ">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-md-6">
+                                Editar producto
+                            </div>
+                            <div class="col-md-6">
+                                <a href="{{route('admin.products')}}" class="btn btn-success pull-right">Todas las productos</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel-body">
+                    @if (Session::has('message'))
+                        <div class="alert alert-success" role="alert">
+                            {{Session::get('message')}}
+                        </div>
+                    @endif
+                    <form class="form-horizontal" wire:submit.prevent="storeCategory" >
+                        <div class="form-group">
+                            <label for="" class="col-md-4 control-label">Nombre del Producto</label>
+                            <div class="col-md-4">
+                                <input type="text" placeholder="Ingrese nombre del Producto" class="form-control input-md">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Slug del producto</label>
+                            <div class="col-md-4">
+                                <input type="text" placeholder="Ingrese el Slug" class="form-control input-md">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Pequrña descripción</label>
+                            <div class="col-md-4">
+                                <textarea class="form-control" name="" placeholder="Ingresar una pequeña descripción"></textarea>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Descripción</label>
+                            <div class="col-md-4">
+                                <textarea class="form-control" name="" placeholder="Ingresar la descripción del producto"></textarea>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Precio de venta</label>
+                            <div class="col-md-4">
+                                <input type="text" placeholder="Ingrese el precio de venta" class="form-control input-md">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Precio de Mayorista</label>
+                            <div class="col-md-4">
+                                <input type="text" placeholder="Ingrese el precio para mayoristas" class="form-control input-md">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">SKU</label>
+                            <div class="col-md-4">
+                                <input type="text" placeholder="Ingrese el SKU" class="form-control input-md">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Stock</label>
+                            <div class="col-md-4">
+                                <select class="form-control">
+                                    <option value="enstock">En-Stock</option>
+                                    <option value="sinstock">Sin-stock</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Imagen del producto</label>
+                            <div class="col-md-4">
+                                <input type="file" class="input-file form-control">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Categoria</label>
+                            <div class="col-md-4">
+                                <select class="form-control">
+                                    <option value="enstock">Seleccionar Categoria</option>
+                                    @foreach ($categorias as $categoria )
+                                        <option value="{{$categoria->id}}">{{$categoria->nombre}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
 use App\Http\Livewire\Admin\AdminAddProductComponent;
+use App\Http\Livewire\Admin\AdminEditProductComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\CategoriaComponent;
@@ -68,6 +69,5 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function () 
     Route::get('/adming/categories/edit/{category_slug}', AdminEditCategoryComponent::class)->name('admin.editcategories');
     Route::get('/admin/products', AdminProductComponent::class)->name('admin.products');
     Route::get('/admin/product/add', AdminAddProductComponent::class)->name('admin.addproduct');
-
-
+    Route::get('/admin/product/edit/{product_slug}', AdminEditProductComponent::class)->name('admin.editproduct');
 });
